38bb59f5873fab79ba9527fae33f506f7904ca87
Maykel <jotasenator@gmail.com>
2023-06-23 21:48:58 -0400

    Add deadline input box in datepicker

--- auctions/forms.py
+++ auctions/forms.py
@@ -5,8 +5,18 @@ from .models import Listing
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
-        fields = ["title", "description", "starting_bid", "image_url", "category"]
-        widgets = {"starting_bid": forms.NumberInput(attrs={"min": 0, "step": 0.01})}
+        fields = [
+            "title",
+            "description",
+            "starting_bid",
+            "image_url",
+            "category",
+            "deadline",
+        ]
+        widgets = {
+            "starting_bid": forms.NumberInput(attrs={"min": 0, "step": 0.01}),
+            "deadline": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        }
 
     def save(self, commit=True):
         listing = super().save(commit=False)
